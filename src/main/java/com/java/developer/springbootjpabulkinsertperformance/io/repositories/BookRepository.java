b0844c4e8b6b4be6e506e945923503079d3b21c1
Luong Vu <hide.dev.2022@gmail.com>
2023-05-10 11:12:55 +0700

    Update Repository

--- src/main/java/com/java/developer/springbootjpabulkinsertperformance/io/repositories/BookRepository.java
+++ src/main/java/com/java/developer/springbootjpabulkinsertperformance/io/repositories/BookRepository.java
@@ -1,11 +1,9 @@
 package com.java.developer.springbootjpabulkinsertperformance.io.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.java.developer.springbootjpabulkinsertperformance.io.entities.Book;
 
-@Repository
 public interface BookRepository extends JpaRepository<Book, String> {
 
 }
